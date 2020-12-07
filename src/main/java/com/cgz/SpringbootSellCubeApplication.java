4b4f1c756c7ae38988ff4e37f1fa380f36b83ed0
caogzh <243509301@qq.com>
2020-12-07 17:36:31 +0800

    修改

--- src/main/java/com/cgz/SpringbootSellCubeApplication.java
+++ src/main/java/com/cgz/SpringbootSellCubeApplication.java
@@ -3,9 +3,11 @@ package com.cgz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.cgz.*.mapper")
+@EnableEurekaClient
 public class SpringbootSellCubeApplication {
 
 	public static void main(String[] args) {
